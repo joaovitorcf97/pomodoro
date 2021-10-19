f0556a71b37822bc5a4c89100a803e7eef37b16f
João Vitor <joaovitorcfdev@gmail.com>
2021-10-19 14:22:35 -0300

    Criando interface base

---
+++ lib/components/cronometro_botao.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CronometroBotao extends StatelessWidget {
+  final String texto;
+  final IconData icone;
+
+  const CronometroBotao({
+    Key? key,
+    required this.texto,
+    required this.icone,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.black,
+        padding: EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 24,
+        ),
+        textStyle: TextStyle(fontSize: 16),
+      ),
+      onPressed: () {},
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: Icon(icone),
+          ),
+          Text(texto),
+        ],
+      ),
+    );
+  }
+}
